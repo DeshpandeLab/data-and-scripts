8177bbfbc5f8a08fb07897645b18923e2c3fd1fb
emilybrxie <128emilyxie@gmail.com>
2023-10-10 15:17:37 -0400

    worked on glmnet regression

--- scripts/glmnet_lasso.py
+++ scripts/glmnet_lasso.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import glmnet
+import scipy as scipy
+import glmnet_python
+from glmnet import glmnet
+from glmnetPrint import glmnetPrint; from glmnetCoef import glmnetCoef
 
 
 #a function that takes two matrices as input and run the glmnet lasso regression
@@ -11,18 +14,21 @@ import glmnet
 #y_pred: Mx1 matrix of what we're looking for, fitted values by the regression
 def glmnet_lasso(gene_list, expression_data):
     
+    #check if parameters are of suitable size matrices
+    
+    
+    #create variables for regression
     X = gene_list
     y = expression_data
     lambdas = np.logspace(np.log10(1e-4), np.log10(35), 100)
 
     #regression
-    enet = glmnet.ElasticNet(lambdas = lambdas)
-    y_pred1 = enet.fit(X, y).predict(X)
-
-    lasso = glmnet.Lasso(lambdas = lambdas)
-    y_pred2 = lasso.fit(X, y).predict(X)
+    fit = glmnet(x = X, y = y, family='gaussian', nlambda=100, alpha=0.2)
+    glmnetPrint(fit)
+    glmnetCoef(fit, s=scipy.float64([0.5]), exact=False)
 
     #decide which model did a better job of fitting and predicting matrix A
+
 
     y_pred = y_pred1 #or y_pred2, depending on which is more optimal
 
